92aad7d3a4442e20582596ebce39c44cd2b512ec
Isaeva-Anna-ngtu22-25 <116427105+Isaeva-Anna-ngtu22-25@users.noreply.github.com>
2023-03-19 21:54:25 +0300

    1.0

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,28 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+    int start = 0;
+    int end = size - 1;
+    int count = 0;
+    while (end >= start) {
+    int middle = (end + start) / 2;
+    if (*(arr + middle) == value) {
+        count += 1;
+        int newmiddle = middle;
+        while ((*(arr + middle + 1)) == value) {
+        count += 1;
+        middle += 1;
+        }
+        while ((*(arr + newmiddle - 1)) == value) {
+        count += 1;
+        newmiddle -= 1;
+        }
+        return count;
+    } else if (*(arr + middle) > value) {
+        end = middle - 1;
+    } else {
+        start = middle + 1;
+    }
+    }
+    return count;
 }
